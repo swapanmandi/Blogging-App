2f94302c4eefd5a643e9d5478d1107121c535956
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-04-17 09:54:46 +0530

    admin file create

---
+++ src/admin/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <div>
+        <ol>
+            <li>SignUp</li>
+            <li>LogIn</li>
+        </ol>
+    </div>
+    
+    </>
+  )
+}
