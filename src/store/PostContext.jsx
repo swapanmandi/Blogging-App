a825134c48abea53f329889d6451c0b8c1392a55
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-04-18 12:41:06 +0530

    setup context api

---
+++ src/store/PostContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+
+
+  export const PostContext = createContext();
+
+
+const  PostContextProvider = ({children}) => {
+
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() =>{
+  const dataFectch = () => {
+    fetch('https://dummyjson.com/quotes').then(response =>{
+      if(!response.ok){
+        console.log(error);
+      }
+      return response.json();
+    }).then( data =>{
+      setPost(data.quotes);
+      console.log('post data are:',post)
+    })
+  };
+  dataFectch();
+
+}, []);
+
+
+  //  const name = 'SWAPAN MANDI';
+  //  const sm = "hi";
+    return(
+    <PostContext.Provider value={{post}}>
+    {children}
+    </PostContext.Provider>
+    );
+};
+
+export default PostContextProvider;
