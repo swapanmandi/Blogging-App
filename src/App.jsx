45df5f4e2c0dd1efe3b6ab68a563bee31e050a01
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-04-18 10:55:40 +0530

    add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import { Outlet } from 'react-router-dom'
 function App() {
   
   return (
+    <PostContextProvider>
     <>
       <Navbar/>
       <Outlet />
       <Footer />
-    </>
-  )
+    </>  
+    </PostContextProvider>
+    )
 }
 
 export default App
