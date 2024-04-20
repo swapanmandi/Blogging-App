3c1c31de3d6b9e3135ff876f7b2ac8dbe8f27eb6
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-04-20 22:19:06 +0530

    add  tailwind css

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return(
         <>
-        <div className="container">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li> <Link to="/blogs">Blogs</Link></li>
-                <li><Link to="/latest">Latest</Link></li>
-                <li> <Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/dashboard">Admin</Link></li>
+        <div className=" bg-red-200 w-full h-16 items-center flex justify-evenly text-md text-blue-600">
+        <div className=" font-bold uppercase">
+            Blogging App
+        </div>
+            <ul className="flex font-semibold ">
+                <li className="mx-[10px]"><Link to="/">Home</Link></li>
+                <li className="mx-[10px]"> <Link to="/blogs">Blogs</Link></li>
+                <li className="mx-[10px]"><Link to="/latest">Latest</Link></li>
+                <li className="mx-[10px]"> <Link to="/about">About</Link></li>
+                <li className="mx-[10px]"><Link to="/contact">Contact</Link></li>
             </ul>
+            <div className=" font-bold bg-red-300 p-1 px-3 rounded-md"> <Link to="/dashboard">Admin</Link> </div>
         </div>
         </>
     )
