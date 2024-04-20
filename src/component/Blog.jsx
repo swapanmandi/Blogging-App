13d22c03f1454706f5f941260f5a25fdfa93ebf0
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-04-20 23:05:16 +0530

    add tailwind css

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -1,22 +1,57 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {PostContext} from '../store/PostContext';
-
+import React, { useContext, useEffect, useState } from "react";
+import { PostContext } from "../store/PostContext";
 
 export default function Blog() {
-
-const {post} = useContext(PostContext);
-   
-  return (
-   <>
-   <h3>Following data are coming from Context Api</h3>
+  const { post } = useContext(PostContext);
+  const { loading } = useContext(PostContext);
   
-   <ul>
-   {
-    post.map(item =>(
-      <li key={item.id}>{item.quote}</li>
-    ))
-   }
-   </ul>
-   </>
-  )
+
+  //for pegination
+
+  const [itemPerPage, setItemPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  const currentItems = post.slice(indexOfFirstItem, indexOfLastItem);
+ 
+  console.log(currentItems)
+
+  const handlePegination = (number) =>{
+
+    setCurrentPage(number);
+  }
+  console.log(post.length);
+
+  const totalPages = post.length / itemPerPage;
+
+  return (
+    <>
+    <div className=" text-slate-100 w-full min-h-screen flex flex-col items-center">
+      <h3>Following data are coming from Context Api</h3>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul className="">
+          {currentItems.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      )}
+
+      <div className=" w-full h-8 bg-slate-200 text-red-900 flex justify-center">
+       
+     {
+     
+     Array.from({length: Math.ceil(post.length / itemPerPage)}, (_, index) =>(
+            <button key={index+1} onClick={() => handlePegination(index+1)} className=" p-1 font-semibold">{index+1}</button>
+          ))
+          
+          }
+          
+      </div>
+      </div>
+    </>
+  );
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,11 +3,37 @@ import {PostContext} from '../store/PostContext';
 
 
 export default function Footer() {
-  const {name} = useContext(PostContext);
+ 
   return (
     <>
-    <div>Footer</div>
-<p>:{name}</p>
+    <footer className=" bg-red-200 w-full h-60 flex justify-between px-8">
+      <div className=' text-black'><h3 className=' font-semibold'>Menu</h3>
+      <div className=' bg-white h-[2px] w-12'></div>
+      <ul>
+        <li>About</li>
+        <li>Blogs</li>
+        <li>Latest</li>
+      </ul>
+      </div>
+
+      <div className=' text-black flex flex-col'><h3 className=' font-semibold'>Latest Blogs</h3>
+      <div className=' bg-white h-[2px] w-15'></div>
+      <ul className=' items-center flex flex-col'>
+        <li>About</li>
+        <li>Blogs</li>
+        <li>Latest</li>
+      </ul>
+      </div>
+
+      <div className=' text-black flex flex-col'><h3 className=' font-semibold'>Scocial</h3>
+      <div className=' bg-white h-[2px] w-12'></div>
+      <ul>
+        <li>About</li>
+        <li>Blogs</li>
+        <li>Latest</li>
+      </ul>
+      </div>
+    </footer>
     </>
   )
 }
