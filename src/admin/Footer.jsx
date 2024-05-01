1a4e8ed7a13da4e0bbc5a8c271367201510d807c
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-01 23:11:52 +0530

    add footer section

--- src/admin/Footer.jsx
+++ src/admin/Footer.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className=' bg-green-300 text-red-500'>
+    <div>
+      <h2>SignUp</h2>
+      <h2>Login</h2>
+    </div>
+    </div>
+    </>
   )
 }
