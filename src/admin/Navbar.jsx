684d4910c1fab91f6e3e44dc7ba37900d09fb9c1
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-01 23:13:28 +0530

    add navlink

--- src/admin/Navbar.jsx
+++ src/admin/Navbar.jsx
@@ -6,8 +6,10 @@ export default function Navbar() {
     <>
     <div className=' bg-stone-50 text-slate-950'>
         <ol className=' flex  justify-center'>
+        <li className=' p-4'><Link to="/dashboard">Home</Link></li>
             <li className=' p-4'> <Link to="/dashboard/signup">SignUp</Link></li>
             <li className=' p-4'><NavLink to="/dashboard/login">LogIn</NavLink></li>
+            <li className=' p-4'><Link to="/">Go to Site</Link></li>
         </ol>
     </div>
     
