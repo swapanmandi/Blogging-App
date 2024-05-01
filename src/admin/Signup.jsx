25227c5b60a4b6f6ddb6920a6e5f2d10b794737d
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-01 23:20:42 +0530

    add signup section

--- src/admin/Signup.jsx
+++ src/admin/Signup.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Signup() {
   return (
-    <div>Signup here</div>
+    <>
+    <div className=' bg-slate-400 h-screen'>
+      <div className=' flex flex-col text-black'>
+      <h1 className=' m-3'>SignUp Goes Here</h1>
+      <label className=' m-5'>Username: <input placeholder='Enter Username'></input></label>
+      
+      <label className=' m-5'>Password:  <input placeholder='Enter Password'></input></label>
+    <label> <button className=' bg-red-400 rounded-md p-2 m-3'>Create</button> </label>
+    </div>
+    </div>
+    </>
   )
 }
