5e4877f043d2cfa39fe4edd2ecf1cae891b123f2
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-02 12:40:41 +0530

    improve pegination button click

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -8,23 +8,41 @@ export default function Blog() {
 
   //for pegination
 
-  const [itemPerPage, setItemPerPage] = useState(5);
+  const [itemPerPage, setItemPerPage] = useState(2);
   const [currentPage, setCurrentPage] = useState(1);
-
+const [pegiList, setPegiList] = useState([])
 
   const indexOfLastItem = currentPage * itemPerPage;
   const indexOfFirstItem = indexOfLastItem - itemPerPage;
   const currentItems = post.slice(indexOfFirstItem, indexOfLastItem);
  
-  console.log(currentItems)
+ 
 
   const handlePegination = (number) =>{
 
     setCurrentPage(number);
   }
-  console.log(post.length);
+
 
   const totalPages = post.length / itemPerPage;
+  const peginatePages = Array.from({length: Math.ceil(totalPages)}, (_, index) => index+1)
+
+useEffect(()=>{
+  if(currentPage> 2 && currentPage < totalPages-1){
+    setPegiList(peginatePages.slice(currentPage-3, currentPage+2))
+  }
+  else if(currentPage === 1){
+    setPegiList(peginatePages.slice(0, 5))
+  }
+  else{
+    
+  }
+},[currentPage])
+ 
+  //const displayPegiPage = 
+  console.log('pegi', pegiList)
+
+  
 
   return (
     <>
@@ -42,14 +60,25 @@ export default function Blog() {
 
       <div className=" w-full h-8 bg-red-200 text-red-900 flex justify-center">
        
-     {
-     
+     {/* {
      Array.from({length: Math.ceil(post.length / itemPerPage)}, (_, index) =>(
             <button key={index+1} onClick={() => handlePegination(index+1)} className=" p-1 font-semibold">{index+1}</button>
           ))
-          
-          }
-          
+          } */}
+
+          <button onClick={ ()=> handlePegination(currentPage-1)}>Previous</button>
+
+{
+  pegiList.map(item => (
+    <div>
+    <button key={item} className=" p-3" onClick={() => handlePegination(item)}>{item}
+    </button>
+    
+    </div>
+  ))
+}
+
+<button onClick={ ()=> handlePegination(currentPage+1)}>Next</button>
       </div>
       </article>
     </>
