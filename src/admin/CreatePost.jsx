6269e63f7f62b0e7b53699c84754aa7fdbf31fc3
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-12 14:22:40 +0530

    modified for submiting data

--- src/admin/CreatePost.jsx
+++ src/admin/CreatePost.jsx
@@ -1,20 +1,41 @@
+import axios from "axios";
 import React, { useState } from "react";
+import ViewPost from "./ViewPost";
 
 export default function Post() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [category, setCategory] = useState("");
+  const [postData, setPostData] = useState({
+    id: 123,
+    title: "first blog",
+    body: "my first post",
+    userId: 45,
+    tags: "#first",
+  });
 
-  //post button handling
+  //post button submit handling
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // onsubmit({ title, content, category });
-    console.log(category ,'-', title , '-', content)
-    setTitle("");
-    setContent("");
-    setCategory("");
+    try {
+      const response = await axios.post("/api/create-post", postData)
+      console.log('Response', response.data)
+      alert(response.data.message)
+    } catch (error) {
+      console.error("Error to post", error);
+      alert('Error to post data')
+    }
+
   };
+
+  //update properties values
+
+  const postHandleChange = (e) => {
+    const { name, value } = e.target;
+    setPostData({
+      ...postData,
+      [name]: value,
+    });
+  };
+  //console.log('input data', postData)
 
   return (
     <>
@@ -22,30 +43,45 @@ export default function Post() {
         <h1 className=" font-bold m-4">Create New Blog</h1>
         <div className=" flex flex-col">
           <form onSubmit={handleSubmit}>
+            <label>Id:
+              <input type="number" name="id" value={postData.id}></input>
+            </label>
             <p>Blog Title</p>
             <input
               type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              name="title"
+              value={postData.title}
+              onChange={postHandleChange}
             ></input>
             <p>Content</p>
             <textarea
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
+              name="body"
+              value={postData.body}
+              onChange={postHandleChange}
               rows={5}
               cols={50}
             ></textarea>
 
-            <label>Category
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value="">---</option>
-              <option value="tech">Tech</option>
-              <option value="travel">Travel</option>
-              <option value="blog">Blog</option>
-            </select>
+            <label>
+              Category
+              <select
+                name="userId"
+                value={postData.userId}
+                onChange={postHandleChange}
+              >
+                <option value="">---</option>
+                <option value="tech">Tech</option>
+                <option value="travel">Travel</option>
+                <option value="blog">Blog</option>
+              </select>
+            </label>
+            <label>
+              Tags
+              <textarea
+                name="tags"
+                value={postData.tags}
+                onChange={postHandleChange}
+              ></textarea>
             </label>
 
             <button className=" bg-orange-500 m-3 rounded-md">Saved</button>
@@ -54,6 +90,7 @@ export default function Post() {
             </button>
           </form>
         </div>
+        <ViewPost />
       </div>
     </>
   );
