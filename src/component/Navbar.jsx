f30d2c796a858c91aafd70bfa0eac0b1ec281a80
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-15 01:18:19 +0530

    create contexApi for dark mode

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,22 +1,55 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import useTheme from "../store/ThemeContext";
+
 const Navbar = () => {
-    return(
-        <>
-        <nav className=" bg-red-200 w-full h-16 items-center flex justify-evenly text-md text-blue-600">
-        <div className=" font-bold uppercase">
-            Blogging App
+  const { themeMode, lightTheme, darkTheme } = useTheme();
+
+  const handleMode = (e) => {
+    const darkModeStatus = e.currentTarget.checked;
+    if (darkModeStatus) {
+      darkTheme();
+    } else {
+      lightTheme();
+    }
+  };
+
+  return (
+    <>
+      <nav className=" bg-red-200 w-full h-16 items-center flex justify-evenly text-md text-blue-600">
+        <div className=" font-bold uppercase">Blogging App</div>
+        <ul className="flex font-semibold ">
+          <li className="mx-[10px]">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="mx-[10px]">
+            {" "}
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li className="mx-[10px]">
+            <Link to="/latest">Latest</Link>
+          </li>
+          <li className="mx-[10px]">
+            {" "}
+            <Link to="/about">About</Link>
+          </li>
+          <li className="mx-[10px]">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className=" font-bold bg-red-300 p-1 px-3 rounded-md">
+          {" "}
+          <Link to="/dashboard">Admin</Link>{" "}
         </div>
-            <ul className="flex font-semibold ">
-                <li className="mx-[10px]"><Link to="/">Home</Link></li>
-                <li className="mx-[10px]"> <Link to="/blogs">Blogs</Link></li>
-                <li className="mx-[10px]"><Link to="/latest">Latest</Link></li>
-                <li className="mx-[10px]"> <Link to="/about">About</Link></li>
-                <li className="mx-[10px]"><Link to="/contact">Contact</Link></li>
-            </ul>
-            <div className=" font-bold bg-red-300 p-1 px-3 rounded-md"> <Link to="/dashboard">Admin</Link> </div>
-        </nav>
-        </>
-    )
-}
+        <input
+          type="checkbox"
+          value=""
+          onChange={handleMode}
+          checked={themeMode === "dark"}
+        />
+      </nav>
+    </>
+  );
+};
 export default Navbar;
