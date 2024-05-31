541ee3a5505bd8f80173a411639c2ced339eb8fd
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-05-31 12:58:30 +0530

    add skeleton loader

---
+++ src/component/MainSidebar.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useEffect, useState } from "react";
+import { PostContext } from "../store/PostContext-store";
+import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+
+export default function MainSidebar({ date = new Date() }) {
+  const { posts, loading } = useContext(PostContext);
+  const todayDate = date.toLocaleDateString();
+  const latestPost = posts.slice(0, 5);
+
+  return (
+    <>
+      {loading ? (
+         <div className=" w-4/12 m-4 rounded-xl">
+        <div className=" bg-white dark:bg-slate-950 min-h-screen p-3 rounded-md">
+         <Skeleton height={30} width={100} /> 
+         {
+        [...Array(6)].map((_, index) => (
+          <div
+            className=" flex p-3 items-center"
+            key={index}
+          >
+        <Skeleton height={64} width={64}/>
+        <div className=" p-4 flex flex-col">
+          <Skeleton height={24} width={60}/>
+          <Skeleton height={24} width={80}/>
+          </div>
+          </div>
+        ))}
+        </div>
+        </div>
+      ) : (
+        <div className=" w-4/12 m-4 rounded-xl">
+          <aside className=" bg-white dark:bg-slate-950 min-h-screen p-3 rounded-md">
+            <div className=" text-red-500 text-wrap">
+              <h2 className="  bg-orange-300 font-semibold rounded-md p-1 w-fit">
+                Trending Posts
+              </h2>
+              {latestPost.map((item) => (
+                <Link to={`/trending-posts/${item.id}`} key={item.id}>
+                  <div className=" flex m-5">
+                    <img
+                      className=" w-[88px] h-[88px] rounded-md"
+                      src={item.image}
+                    ></img>
+                    <div>
+                      <h2 className=" m-4">{item.title}</h2>
+                      <span className=" m-4">📅 {todayDate}</span>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </aside>
+        </div>
+      )}
+    </>
+  );
+}
--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PostContext } from "../store/PostContext-store";
 import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function PostList() {
   //for geting data
-  const { posts } = useContext(PostContext);
-  const { loading } = useContext(PostContext);
+  const { posts, loading } = useContext(PostContext);
+
   const [currDate, setCurrDate] = useState(new Date());
 
   //for pegination
@@ -30,72 +32,93 @@ export default function PostList() {
 
   useEffect(() => {
     if (totalPages < 5 || currentPage < 5) {
-      setPeginateList(peginatePages.slice(0, 5)); 
-    }
-    else  if ( currentPage > totalPages - 4) {
-      setPeginateList(peginatePages.slice( totalPages -5, totalPages)); 
-    }
-    else
-     {
+      setPeginateList(peginatePages.slice(0, 5));
+    } else if (currentPage > totalPages - 4) {
+      setPeginateList(peginatePages.slice(totalPages - 5, totalPages));
+    } else {
       setPeginateList(peginatePages.slice(currentPage - 3, currentPage + 2));
     }
   }, [currentPage, totalPages]);
 
   return (
     <>
-      <div className=" flex flex-col items-center">
-        {currentItems.map((item) => (
-          <Link to={`blogs/${item.id}`}>
-            <div
-              className="  bg-white flex dark:bg-slate-950 dark:text-white p-4 text-md m-4 rounded-xl"
-              key={item.id}
-            >
-              <img className=" w-[324px] h-[281px]" src={`${item.image}`}></img>
-              <div className=" p-3">
-                <span className=" bg-amber-400 rounded-md p-2 font-semibold">
-                  {item.tags}
-                </span>
-                <div className=" p-4">
-                  <h1 className=" text-xl font-semibold p-3">{item.title}</h1>
-                  <span className=" p-3">
-                    📅 {currDate.toLocaleDateString()}
-                  </span>
-                  <span className="p-3">👨 Admin</span>
-                  <span className="p-3">💬 0</span>
-                </div>
-                <p>{item.body}</p>
-                <h3 className=" font-semibold">Read More</h3>
+      {loading ? (
+        [...Array(5)].map((_, index) => (
+          <div
+            className="bg-white flex dark:bg-slate-950 dark:text-white p-4 text-md m-4 rounded-xl"
+            key={index}
+          >
+            <Skeleton width={324} height={281} />
+            <div className="p-3 flex flex-col w-full">
+              <Skeleton height={30} width={100} />
+              <div className="p-4">
+                <Skeleton height={25} width="60%" />
+                <Skeleton height={20} width="40%" />
+                <Skeleton height={20} width="40%" />
+                <Skeleton height={20} width="40%" />
               </div>
+              <Skeleton height={20} width="80%" />
+              <Skeleton height={20} width="40%" />
             </div>
-          </Link>
-        ))}
-       
-        <div className=" w-fit p-2 m-4 dark:text-white">
-          <button
-            disabled={currentPage === 1}
-            onClick={() => handlePeginate(currentPage - 1)}
-            className="m-2 font-semibold disabled:text-gray-400"
-          >
-            {`<<`}Previous
-          </button>
-          {peginateList.map((item) => (
-            <button 
-              key={item}
-              className={`p-2  bg-slate-400 font-semibold m-1 px-3 ${item === currentPage && "text-red-600"}`}
-              onClick={() => handlePeginate(item)}
-            >
-              {item}
-            </button>
+          </div>
+        ))
+      ) : (
+        <div className=" flex flex-col items-center">
+          {currentItems.map((item) => (
+            <Link to={`blogs/${item.id}`} key={item.id}>
+              <div className="  bg-white flex dark:bg-slate-950 dark:text-white p-4 text-md m-4 rounded-xl">
+                <img
+                  className=" w-[324px] h-[281px]"
+                  src={`${item.image}`}
+                ></img>
+                <div className=" p-3">
+                  <span className=" bg-amber-400 rounded-md p-2 font-semibold">
+                    {item.tags}
+                  </span>
+                  <div className=" p-4">
+                    <h1 className=" text-xl font-semibold p-3">{item.title}</h1>
+                    <span className=" p-3">
+                      📅 {currDate.toLocaleDateString()}
+                    </span>
+                    <span className="p-3">👨 Admin</span>
+                    <span className="p-3">💬 0</span>
+                  </div>
+                  <p>{item.body}</p>
+                  <h3 className=" font-semibold">Read More</h3>
+                </div>
+              </div>
+            </Link>
           ))}
-          <button
-            disabled={currentPage === totalPages}
-            onClick={() => handlePeginate(currentPage + 1)}
-            className="m-2 font-semibold disabled:text-gray-400"
-          >
-            Next{`>>`}
-          </button>
+
+          <div className=" w-fit p-2 m-4 dark:text-white">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => handlePeginate(currentPage - 1)}
+              className="m-2 font-semibold disabled:text-gray-400"
+            >
+              {`<<`}Previous
+            </button>
+            {peginateList.map((item) => (
+              <button
+                key={item}
+                className={`p-2  bg-slate-400 font-semibold m-1 px-3 ${
+                  item === currentPage && "text-red-600"
+                }`}
+                onClick={() => handlePeginate(item)}
+              >
+                {item}
+              </button>
+            ))}
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => handlePeginate(currentPage + 1)}
+              className="m-2 font-semibold disabled:text-gray-400"
+            >
+              Next{`>>`}
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
