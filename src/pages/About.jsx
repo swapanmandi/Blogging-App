5cf04fedeb7539ee7f6904536fac71242ef5c428
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-06-25 07:37:14 +0530

    set up the page

---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <>
+    <div>
+        
+<div>
+
+<h1>About<b>Blogging App</b></h1>
+<p> Welcome to <b>Blogging App</b> where we share information related to . We're dedicated to providing you the very best information and knowledge of the above mentioned topics. Our about us page is generated with the help of <a href="https://raptorkit.com/about-us-page-generator" >About Us Page Generator</a></p>
+
+<p>We hope you found all of the information on <b>Blogging App</b> helpful, as we love to share them with you.</p>
+
+<p>If you require any more information or have any questions about our site, please feel free to contact us by email at <b>bloggingapp@gmail.com</b>.</p>
+
+
+
+</div>
+	
+    </div>
+    
+    </>
+  )
+}
