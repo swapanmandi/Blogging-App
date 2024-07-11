4075ce1f372930150cd90277a657f189c422f018
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-07-11 23:33:05 +0530

    create blog model

---
+++ Backend/src/models/blog.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+
+const blogSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    slug:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    featuredImage:{
+        type: String
+    },
+    category:{
+        type:String
+    },
+    status:{
+        type: String,
+        enum:['active', 'inactive']
+    }
+},{timestamps:true})
+
+export const Blog = mongoose.model("Blog", blogSchema)
