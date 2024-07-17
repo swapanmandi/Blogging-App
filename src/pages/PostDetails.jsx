7e5a0a454475756ec94c61e4fa18d23d026765be
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-07-18 02:58:27 +0530

    parser the content

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import MainSidebar from "../component/MainSidebar";
 import RelatedPosts from "../component/RelatedPosts";
 import SocialShare from "../component/SocialShare";
+import parse from "html-react-parser";
 
 export default function Post() {
   const { posts } = useContext(PostContext);
@@ -16,11 +17,13 @@ export default function Post() {
 
   const nevigate = useNavigate();
 
+  console.log("posts", posts);
+
   useEffect(() => {
     setData(posts);
   }, [posts]);
 
-  const viewPost = data.filter((item) => String(item.id) === postId);
+  const viewPost = data.filter((item) => String(item._id) === postId);
   const currentIndex = data.findIndex((item) => String(item.id) === postId);
 
   const handlePostChange = (id) => {
@@ -37,15 +40,19 @@ export default function Post() {
     }
   };
 
-  console.log('commt', viewComment)
+  console.log("commt", viewComment);
+  const sm = '<h1>swapan</h1>'
 
   return (
     <>
       <div className="flex">
         <div className=" bg-white dark:bg-slate-950 dark:text-white w-8/12  m-4 p-5">
           {viewPost.map((item) => (
-            <div className="" key={item.id}>
-              <img className=" w-[760px] h-[490px] mb-5" src={item.image}></img>
+            <div className="" key={item._id}>
+              <img
+                className=" w-[760px] h-[490px] mb-5"
+                src={item.featuredImage}
+              ></img>
               <span className=" bg-orange-400 rounded-lg p-1">{item.tags}</span>
 
               <SocialShare
@@ -58,23 +65,35 @@ export default function Post() {
                 <span className=" p-3">📅 {item.publishedAt}</span>
                 <span className="p-3">👨 Admin</span>
               </div>
-              <p>{item.body}</p>
+
+              {/* -------------content--------------- */}
+              <div>
+                {parse(item.content)}
+              </div>
+              
               <div className=" m-3">
                 <span className=" p-3">👍{item.likes?.length}</span>
                 <span className=" p-3">↪️{item.shares?.length}</span>
-                <span className="p-3 cursor-pointer" onClick={clickCommentHandler}> 💬 {item.comments?.length}</span>
+                <span
+                  className="p-3 cursor-pointer"
+                  onClick={clickCommentHandler}
+                >
+                  {" "}
+                  💬 {item.comments?.length}
+                </span>
               </div>
-{ viewComment &&
-              <div className=" m-3 p-1 border-slate-400 border-2 rounded-md">
-                <h3 className=" font-semibold p-3">Comments</h3>
-                {item.comments?.length ?
-              (item.comments?.map((item) => (
-                  <div className="flex items-center">
-                    {item.userId}:<p className=" p-3">{item.comment}</p>
-                  </div>
-                ))) : "No Comments!" }
-              </div>
-}
+              {viewComment && (
+                <div className=" m-3 p-1 border-slate-400 border-2 rounded-md">
+                  <h3 className=" font-semibold p-3">Comments</h3>
+                  {item.comments?.length
+                    ? item.comments?.map((item, index) => (
+                        <div key={index} className="flex items-center">
+                          {item.userId}:<p className=" p-3">{item.comment}</p>
+                        </div>
+                      ))
+                    : "No Comments!"}
+                </div>
+              )}
             </div>
           ))}
 
