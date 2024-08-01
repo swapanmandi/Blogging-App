222cbac1495687420ae3e93467e0c41a766cb841
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-08-01 23:58:58 +0530

    update loading state and dependecy array

--- src/store/PostContext-store.jsx
+++ src/store/PostContext-store.jsx
@@ -7,21 +7,24 @@ const PostContextProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await new Promise(resolve => setTimeout(resolve, 1000))
-        const response = await axios.get("../src/../backend/posts.json");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        //const response = await axios.get("../src/../backend/posts.json");
+        const response = await axios.get("http://localhost:3000/blog/list", {
+          withCredentials: false,
+        });
+        console.log("post api data", response.data.data);
+        setPosts(response.data.data);
 
-        setPosts(response.data);
-
-        setLoading(false);
       } catch {
         (error) => {
           console.error("There are something wrong to get data.", error);
-          setLoading(false);
         };
+      }
+      finally{
+        setLoading(false)
       }
     };
 
@@ -29,7 +32,7 @@ const PostContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <PostContext.Provider value={{ posts, loading, setLoading }}>
+    <PostContext.Provider value={{ posts, setPosts, loading, setLoading }}>
       {children}
     </PostContext.Provider>
   );
