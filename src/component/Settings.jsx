2c9b945b950372acffba8851edc02fd9f02c576a
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-08-23 19:38:58 +0530

    update avatar preview

--- src/component/Settings.jsx
+++ src/component/Settings.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../store/AuthContext.jsx";
 
 export default function Settings() {
-  const [profilePic, setProfilePic] = useState(null);
   const [accountInfo, setAccountInfo] = useState([]);
   const [isClickedAccInfo, setIsClickedAccInfo] = useState(false);
   const [isEditingAccInfo, setIsEditingAccInfo] = useState(false);
@@ -21,7 +22,14 @@ export default function Settings() {
     }
   }, [accountInfo]);
 
-  const [avatarView, setAvatarView] = useState(null);
+  const [avatarPreview, setAvatarPreview] = useState(null);
+  const [isClickedGeneralSettings, setIsClickedGeneralSettings] =
+    useState(false);
+  const [isEditingPassword, setIsEditingPassword] = useState(false);
+
+  const { register, resetField, handleSubmit } = useForm();
+
+  const { signout } = useContext(AuthContext);
 
   const handlePerosonalInfo = async () => {
     setIsClickedAccInfo(true);
@@ -30,15 +38,12 @@ export default function Settings() {
       withCredentials: true,
     });
     setAccountInfo(result.data.data);
-    console.log(accountInfo.avatar);
   };
-
-  const handleSignOut = () => {};
 
   const handleCloseAccInfo = () => {
     setIsClickedAccInfo(false);
     setShowDropdown(true);
-    setAvatarView(null)
+    setAvatarPreview(null);
   };
 
   const handleEditAccInfo = () => {
@@ -71,39 +76,63 @@ export default function Settings() {
     }
   };
 
-  const handleProfilePic = (e) => {
+  const handleViewAvatar = (e) => {
     const file = e.target.files[0];
-    setProfilePic(file);
-
     if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setAvatarView(reader.result);
-      };
-      reader.readAsDataURL(file);
+      setAvatarPreview(URL.createObjectURL(file));
     }
   };
-
-  console.log("dp", profilePic);
-
-  const handleAvatarUpload = async (e) => {
-    e.preventDefault();
-
+  
+  const handleAvatarUpload = async (data) => {
     const formData = new FormData();
-    formData.append("avatar", profilePic);
+    formData.append("avatar", data.avatar[0]);
 
-    const data = {
-      avatar: formData.get("avatar"),
-    };
+    // const avatarFile = {
+    //   avatar: formData.get("avatar"),
+    // };
 
-    console.log("data", data.avatar);
+    // for (let [key, value] of formData.entries()) {
+    //   console.log(`${key}:`, value);
+    // }
 
-    await axios.post("http://localhost:3000/avatar", data, {
+    await axios.post("http://localhost:3000/avatar", formData, {
       withCredentials: true,
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
+  };
+
+  const handleGeneralSettings = () => {
+    setIsClickedGeneralSettings(true);
+    setShowDropdown(false);
+  };
+
+  const handleCloseGeneralSettins = () => {
+    setIsClickedGeneralSettings(false);
+    setShowDropdown(true);
+  };
+
+  const handlePasswordEdit = () => {
+    setIsEditingPassword(true);
+  };
+
+  const handleCancelEditingPassword = () => {
+    setIsEditingPassword(false);
+    resetField("oldPassword");
+    resetField("newPassword");
+  };
+
+  const handleSaveEditingPassword = async (data) => {
+    try {
+      await axios.post("http://localhost:3000/password-change", data, {
+        withCredentials: true,
+      });
+      resetField("oldPassword");
+      resetField("newPassword");
+    } catch (error) {
+      console.error("Error to save new Password.", error);
+    }
   };
 
   return (
@@ -119,11 +148,17 @@ export default function Settings() {
               >
                 Personal Information
               </li>
+              <li
+                onClick={handleGeneralSettings}
+                className=" bg-indigo-700 p-2 rounded-md m-2"
+              >
+                General Settings
+              </li>
               <li className=" bg-indigo-700 p-2 rounded-md m-2">
                 Delete Account
               </li>
               <li
-                onClick={handleSignOut}
+                onClick={signout}
                 className=" bg-indigo-700 p-2 rounded-md m-2"
               >
                 Sign out
@@ -133,13 +168,13 @@ export default function Settings() {
         )}
 
         {isClickedAccInfo && (
-          <div className=" bg-amber-200 h-1/2 w-1/2">
+          <div className=" bg-amber-200 h-fit w-1/2 overflow-hidden">
             <h2>Account Info</h2>
             <div className=" h-32 w-32 bg-slate-200 rounded-full overflow-hidden">
-              {avatarView || accountInfo.avatar ? (
+              {avatarPreview || accountInfo.avatar ? (
                 <img
                   className=" h-full w-full object-cover"
-                  src={avatarView ? avatarView : accountInfo.avatar}
+                  src={avatarPreview ? avatarPreview : accountInfo.avatar}
                 ></img>
               ) : (
                 <span className="flex justify-center items-center">
@@ -148,78 +183,161 @@ export default function Settings() {
               )}
             </div>
 
-            <form onSubmit={handleAvatarUpload}>
+            <form onSubmit={handleSubmit(handleAvatarUpload)}>
               <input
                 type="file"
                 accept="image/png, image/jpg, image/jpeg, image/gif"
-                onChange={handleProfilePic}
-              ></input>
+                {...register("avatar", { required: true })}
+                onChange={handleViewAvatar}
+              />
               <button className=" p-2 m-2 rounded-md bg-lime-500" type="submit">
                 Save
               </button>
             </form>
 
-            {isEditingAccInfo ? (
-              <div className=" bg-teal-200 flex justify-center items-center">
-                <div className=" flex flex-col">
-                  <label>
-                    Full Name:
-                    <input
-                      className=" text-black m-2 p-1"
-                      name="fullName"
-                      onChange={handleOnChangeAccInfo}
-                      value={editingAccInfo.fullName}
-                    ></input>
-                  </label>
-
-                  <label>
-                    Email:
-                    <input
-                      className=" text-black m-2 p-1"
-                      name="email"
-                      onChange={handleOnChangeAccInfo}
-                      value={editingAccInfo.email}
-                    ></input>
-                  </label>
-
-                  <div>
-                    <button
-                      className=" bg-lime-500 p-2 rounded-md m-3"
-                      onClick={handleCancelEditingAccInfo}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className=" bg-lime-500 p-2 rounded-md m-3"
-                      onClick={handleSaveAccInfo}
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className=" absolute w-60 h-60 text-black">
+            <div className=" bg-emerald-100 flex w-60 h-fit text-black">
+              <div>
                 <h2>Name: {accountInfo.fullName}</h2>
                 <h2>Email: {accountInfo.email}</h2>
+              </div>
 
-                <div>
-                  <button
-                    onClick={handleCloseAccInfo}
-                    className=" bg-lime-500 p-2 rounded-md m-2"
-                  >
-                    Close
-                  </button>
-                  <button
-                    onClick={handleEditAccInfo}
-                    className=" bg-lime-500 p-2 rounded-md m-2"
-                  >
-                    Edit
-                  </button>
+              <button
+                onClick={handleEditAccInfo}
+                className=" bg-lime-500 p-2 rounded-md m-2"
+              >
+                Edit
+              </button>
+
+              {isEditingAccInfo && (
+                <div className="  absolute bg-teal-200 flex justify-center items-center">
+                  <div className=" flex flex-col">
+                    <label>
+                      Full Name:
+                      <input
+                        className=" text-black m-2 p-1"
+                        name="fullName"
+                        onChange={handleOnChangeAccInfo}
+                        value={editingAccInfo.fullName}
+                      ></input>
+                    </label>
+
+                    <label>
+                      Email:
+                      <input
+                        className=" text-black m-2 p-1"
+                        name="email"
+                        onChange={handleOnChangeAccInfo}
+                        value={editingAccInfo.email}
+                      ></input>
+                    </label>
+
+                    <div>
+                      <button
+                        className=" bg-lime-500 p-2 rounded-md m-3"
+                        onClick={handleCancelEditingAccInfo}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        className=" bg-lime-500 p-2 rounded-md m-3"
+                        onClick={handleSaveAccInfo}
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
                 </div>
+              )}
+            </div>
+
+            <div className=" text-black flex">
+              <div>
+                Password:
+                <input
+                  className=" bg-white"
+                  placeholder="*******"
+                  disabled
+                ></input>
+              </div>
+              <button
+                className="-top-2 bg-lime-500 p-2 rounded-md m-2"
+                onClick={handlePasswordEdit}
+              >
+                Change
+              </button>
+            </div>
+
+            {isEditingPassword && (
+              <div className=" absolute bg-green-300">
+                <div>
+                  Edit Password
+                  <form onSubmit={handleSubmit(handleSaveEditingPassword)}>
+                    <label>
+                      Old Password:
+                      <input
+                        autoComplete="off"
+                        className=" bg-cyan-600 rounded-md w-72 outline-none px-2 m-2"
+                        type="password"
+                        {...register("oldPassword", {
+                          required: true,
+                          minLength: 8,
+                          maxLength: 20,
+                        })}
+                      />
+                    </label>
+                    <label>
+                      New Password:
+                      <input
+                        autoComplete="on"
+                        className=" bg-cyan-600 rounded-md w-72 outline-none px-2 m-2"
+                        type="password"
+                        {...register("newPassword", {
+                          required: true,
+                          minLength: 8,
+                          maxLength: 20,
+                        })}
+                      />
+                    </label>
+                    <input
+                      className="-top-2 bg-lime-500 p-2 rounded-md m-2"
+                      type="submit"
+                    />
+                  </form>
+                </div>
+                <button
+                  onClick={handleCancelEditingPassword}
+                  className="-top-2 bg-lime-500 p-2 rounded-md m-2"
+                >
+                  Cancel
+                </button>
+
+                <div></div>
               </div>
             )}
+
+            <button
+              onClick={handleCloseAccInfo}
+              className="-top-2 bg-lime-500 p-2 rounded-md m-2"
+            >
+              Close
+            </button>
           </div>
+        )}
+
+        {isClickedGeneralSettings && (
+          <>
+            <div className=" bg-red-300 h-1/2 w-1/2">
+              General Settings
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <div>
+                <button onClick={handleCloseGeneralSettins}>Close</button>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </>
