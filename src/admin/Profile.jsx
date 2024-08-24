3070d50de1c7ba1a4926be102dbe5832e1c8159f
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-08-24 23:15:54 +0530

    update the edit data

--- src/admin/Profile.jsx
+++ src/admin/Profile.jsx
@@ -1,10 +1,127 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../store/AuthContext.jsx";
 
 export default function Profile() {
+  const [avatar, setAvatar] = useState(null);
+  const [showMenu, setShowMenu] = useState(true);
+  const [showAccInfo, setShowAccInfo] = useState(false);
+  const [editingPersonalInfo, setEditingPersonalInfo] = useState(false);
+  const [editingPassword, setEditingPassword] = useState(false);
   const [profileInfo, setProfileInfo] = useState(null);
+
+  const { register: registerAvatar, handleSubmit: handleSubmitAvatar } =
+    useForm();
+  const {
+    register: registerPersonalInfo,
+    handleSubmit: handleSubmitPersonalInfo,
+    reset,
+  } = useForm();
+
+  const { register: registerPassword, handleSubmit: handleSubmitPassword } =
+    useForm();
+
+  const { signout } = useContext(AuthContext);
+
+  const clickAccInfo = () => {
+    setShowAccInfo(true);
+    setShowMenu(false);
+  };
+
+  const closeAccInfo = () => {
+    setShowAccInfo(false);
+    setShowMenu(true);
+  };
+
+  const handleAvatrPreview = (e) => {
+    const file = e.target.files[0];
+    setAvatar(URL.createObjectURL(file));
+
+    // const file = e.target.files[0]
+    // if(file){
+    //   const reader = new FileReader()
+    //   reader.onloadend = () =>{
+    //     setAvatar(reader.result)
+    //   }
+    //   reader.readAsDataURL(file)
+    // }
+  };
+
+  const updateAvatar = async (data) => {
+    const formData = new FormData();
+    setAvatar(data.avatar[0]);
+
+    formData.append("avatar", data.avatar[0]);
+
+    await axios.post("http://localhost:3000/avatar", formData, {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  };
+
+  const editPersonanInfo = () => {
+    if (profileInfo?.fullName && profileInfo?.email) {
+      reset({
+        fullName: profileInfo?.fullName,
+        email: profileInfo?.email,
+      });
+    }
+    setEditingPersonalInfo(true);
+    setShowAccInfo(false);
+  };
+
+  const cancelEditPersonalInfo = () => {
+    setEditingPersonalInfo(false);
+    setShowAccInfo(true);
+  };
+
+  const SaveEditPersonalInfo = async (data) => {
+    try {
+      const result = await axios.post(
+        "http://localhost:3000/account-update",
+        { fullName: data.fullName, email: data.email },
+        {
+          withCredentials: true,
+        }
+      );
+
+      setEditingPersonalInfo(false);
+      setShowAccInfo(true);
+    } catch (error) {
+      console.error("ERROR to Save Personal Info.");
+    }
+  };
+
+  const editPassowrd = () => {
+    setEditingPassword(true);
+    setShowAccInfo(false);
+  };
+
+  const saveEditPassword = async (data) => {
+    try {
+      await axios.post(
+        "http://localhost:3000/password-change",
+        { oldPassword: data.oldPassword, newPassword: data.newPassword },
+        {
+          withCredentials: true,
+        }
+      );
+      setEditingPassword(false);
+      setShowAccInfo(true);
+    } catch (error) {
+      console.error("Error to save password.", error);
+    }
+  };
+
+  const cancelEditPassword = () => {
+    setEditingPassword(false);
+    setShowAccInfo(true);
+  };
 
   useEffect(() => {
     const profile = async () => {
@@ -12,18 +129,173 @@ export default function Profile() {
         withCredentials: true,
       });
       setProfileInfo(result.data.data);
-      console.log("profile data", result.data.data);
       toast(result.data?.message);
     };
     profile();
   }, []);
   return (
     <>
-      <div className=" w-full items-center flex justify-center">
-        <div className="bg-gray-600 w-2/5 h-screen">
-          <h2 className="p-3">Name: {profileInfo?.fullName}</h2>
-          <h2 className="p-3">Email: {profileInfo?.email}</h2>
-        </div>
+      <div className=" w-full items-center flex justify-center overflow-hidden">
+        {showMenu && (
+          <ul className=" bg-neutral-400 min-h-60 w-1/2 rounded-md">
+            <li
+              onClick={clickAccInfo}
+              className=" bg-slate-500 w-fit p-1 m-2 rounded-md cursor-pointer"
+            >
+              Account Info
+            </li>
+            <li className=" bg-slate-500 w-fit p-1 m-2 rounded-md cursor-pointer">
+              General Settings
+            </li>
+            <li
+              onClick={signout}
+              className=" bg-slate-500 w-fit p-1 m-2 rounded-md cursor-pointer"
+            >
+              Sign Out
+            </li>
+          </ul>
+        )}
+
+        {/* first modal */}
+
+        {showAccInfo && (
+          <div className=" absolute p-3 rounded-md bg-slate-200">
+            <div className=" flex flex-col justify-center items-center">
+              <div className=" overflow-hidden flex justify-center items-center  bg-slate-100 h-24 w-24 rounded-full m-2">
+                {avatar || profileInfo?.avatar ? (
+                  <img
+                    src={avatar ? avatar : profileInfo?.avatar}
+                    className=" h-full w-full object-cover"
+                  ></img>
+                ) : (
+                  "No Avatar"
+                )}
+              </div>
+              <form onSubmit={handleSubmitAvatar(updateAvatar)}>
+                <input
+                  type="file"
+                  {...registerAvatar("avatar", { required: true })}
+                  onChange={handleAvatrPreview}
+                />
+
+                <button
+                  type="submit"
+                  className="bg-slate-500 h-fit w-fit p-1 rounded-md"
+                >
+                  Update
+                </button>
+              </form>
+
+              <div className=" w-full m-2 flex justify-between items-center">
+                <div className="">
+                <h2>Name: {profileInfo?.fullName} </h2>
+                <h2>Email: {profileInfo?.email} </h2>
+
+                  </div>
+                <button
+                className=" bg-slate-500 h-fit w-fit p-1 rounded-md "
+                onClick={editPersonanInfo}
+              >
+                Edit
+              </button>
+              </div>
+              
+            </div>
+            <div className=" flex justify-between">
+              <h2>
+                Password: <input className="" placeholder="******" disabled></input>
+              </h2>
+              <button
+                onClick={editPassowrd}
+                className=" bg-slate-500 w-fit p-1 rounded-md "
+              >
+                Change
+              </button>
+            </div>
+
+            <button
+              className=" bg-slate-500 w-fit p-1 rounded-md "
+              onClick={closeAccInfo}
+            >
+              Close
+            </button>
+          </div>
+        )}
+
+        {/* second modal */}
+
+        {editingPersonalInfo && (
+          <div className=" absolute h-fit w-fit bg-orange-400 p-1 flex flex-col rounded-md shadow-lg ">
+            <form onSubmit={handleSubmitPersonalInfo(SaveEditPersonalInfo)}>
+              <input
+                className="w-full mt-1 p-2 border border-gray-300 rounded-md "
+                type="text"
+                {...registerPersonalInfo("fullName", { required: true })}
+              />
+              <input
+                className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+                type="email"
+                {...registerPersonalInfo("email", { required: true })}
+              />
+
+              <button
+                onClick={cancelEditPersonalInfo}
+                type="button"
+                className=" bg-gray-300  h-fit w-fit p-2 m-2 rounded-md "
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="bg-blue-500  h-fit w-fit p-2 m-2 rounded-md "
+              >
+                Save
+              </button>
+            </form>
+          </div>
+        )}
+
+        {/* third modal */}
+
+        {editingPassword && (
+          <div className=" absolute bg-lime-300 p-2  rounded-md">
+           
+            <form onSubmit={handleSubmitPassword(saveEditPassword)}
+            className=" flex flex-col">
+              <label>
+                Old Password:
+                <input
+                  type="text"
+                  {...registerPassword("oldPassword", { required: true })}
+                  className=" m-2 rounded-md p-1 outline-none"
+                />
+              </label>
+              <label>
+                New Passowrd:
+                <input
+                  type="text"
+                  {...registerPassword("newPassword", { required: true })}
+                  className=" m-2 rounded-md p-1 outline-none"
+                />
+                <div className=" flex justify-end">
+                  <button
+                    type="button"
+                    onClick={cancelEditPassword}
+                    className=" bg-slate-400 w-fit p-1 rounded-md m-2"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className=" bg-blue-400 w-fit p-1 rounded-md m-2"
+                  >
+                    Save
+                  </button>
+                </div>
+              </label>
+            </form>
+          </div>
+        )}
       </div>
     </>
   );
