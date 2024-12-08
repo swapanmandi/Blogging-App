2b5278bf64a659280c0ccaaf76d736dd17688b3b
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-12-08 11:18:25 +0530

    update ui and add  fetch category

--- src/admin/Category.jsx
+++ src/admin/Category.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { addCategory, getCategory } from "../api";
 
 export default function Category() {
   const [category, setCategory] = useState("");
@@ -7,76 +8,66 @@ export default function Category() {
 
   useEffect(() => {
     const getCategories = async () => {
-      const result = await axios.get(`${import.meta.env.VITE_BACKEND_API}/api/v1/blog/get/category`, {
-        withCredentials: true,
-      });
-
+      const result = await getCategory();
       setCategories(result.data.data);
     };
     getCategories();
-  }, [category]);
+  }, []);
 
   const handleAddCategory = async () => {
-    const result = await axios.post(
-      `${import.meta.env.VITE_BACKEND_API}/api/v1/blog/create/category`,
-      { category },
-      {
-        withCredentials: true,
-      }
-    );
+    const result = await addCategory({ category });
+    setCategories([result.data.data]);
 
     setCategory("");
   };
-  console.log("category", category);
-  console.log("categories", categories);
+  // console.log("category", category);
+  // console.log("categories", categories);
 
   const handleCategoryInput = (e) => {
     setCategory(e.target.value);
   };
 
-  const handleRemoveCdategory = (e) => {
-
-  };
+  const handleRemoveCdategory = (e) => {};
 
   return (
-    <>
-      <div className=" flex space-x-10">
-        <div>
-          <h2>Add Category</h2>
-          <div>
-            <input
-              className=" text-black bg-slate-400"
-              onChange={handleCategoryInput}
-              value={category}
-            ></input>
-            <button
-              type="button"
-              onClick={handleAddCategory}
-              className=" bg-red-500 rounded-md p-1 m-2"
-            >
-              Add
-            </button>
-          </div>
+    <div className=" w-full p-2 flex justify-center">
+      <div className=" bg-orange-300 w-1/2 h-fit flex flex-col items-center rounded-md">
+        <h2 className=" m-2">Add Category</h2>
+        <div className=" w-full flex items-center justify-center">
+          <input
+            className=" w-2/4 h-fit text-black bg-slate-400 rounded-sm outline-none p-1"
+            onChange={handleCategoryInput}
+            value={category}
+          />
+          <button
+            type="button"
+            onClick={handleAddCategory}
+            className=" bg-red-500 rounded-md p-1 m-2"
+          >
+            Add
+          </button>
         </div>
-        <div className="">
+        <div className=" w-fit ">
           {categories?.map((item, index) => (
-            <div className=" bg-red-100 h-fit w-40 flex flex-col justify-center items-center " key={index}>
-              
-                {item.category.map(item => (
-                  <div className=" m-3" key={item}>
-                    <span className=" bg-red-400 p-1 m-1 rounded-md">{item}</span>
-                    <span
-                className=" w-6 text-center h-8 p-1 rounded-md bg-red-400 cursor-pointer"
-                onClick={() => handleRemoveCdategory(item)}
-              >
-                X
-              </span>
-                    </div>
-                ))}
-              </div>
+            <div
+              className="  h-fit w-40 flex flex-col justify-center items-center "
+              key={index}
+            >
+              {item.category.map((item) => (
+                <div className=" m-3" key={item}>
+                  <span className=" bg-red-400 p-1 m-1 rounded-md">{item}</span>
+                  <span
+                    className=" w-6 text-center h-8 p-1 rounded-md bg-red-400 cursor-pointer"
+                    onClick={() => handleRemoveCdategory(item)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+            </div>
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
