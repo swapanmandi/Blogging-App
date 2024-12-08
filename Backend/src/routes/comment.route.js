57b2a031498bc82a4cad16aacabcca10ea8377b9
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-12-08 11:19:11 +0530

    update route and add fetch comment count route

--- Backend/src/routes/comment.route.js
+++ Backend/src/routes/comment.route.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { addComment, getComments } from "../controllers/comment.controller.js";
+import {
+  addComment,
+  getComments,
+  getCommentDashboardData,
+} from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/post/comment/:id").post(verifyJWT, addComment);
-router.route("/post/comments/:id").get(getComments)
-
+router.route("/post/comments/:id").get(getComments);
+router.route("/comments-count").get(verifyJWT, getCommentDashboardData);
 
 export default router;
