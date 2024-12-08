49f16e5424d0bca99d1ede1dda9c2fc83301fd2b
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-12-08 12:25:14 +0530

    update proxy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server:{
     proxy:{
-      '/api' : "http://localhost:3000",
+      '/api' : "https://blogpress-chiu.onrender.com",
     },
   },
   plugins: [react()],
