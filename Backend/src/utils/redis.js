b9f0ce093ad216d97bc378a37bf9bd288184e647
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-12-08 11:24:01 +0530

    update the credentials

--- Backend/src/utils/redis.js
+++ Backend/src/utils/redis.js
@@ -1,21 +1,11 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  password: "nNfmacQ5KXHGSjsepfxPUto4gcu3QpeT",
-  host: "redis-11190.c278.us-east-1-4.ec2.redns.redis-cloud.com",
-  port: 11190,
+  password: 'Xg6lhNqqdCGL0OnsEIY1S9TqZraRLAFV',
+  host: 'redis-12308.c326.us-east-1-3.ec2.redns.redis-cloud.com',
+  port: 12308,
   username: "default",
   db: 0,
 });
 
 export default redis;
-
-// import { createClient } from 'redis';
-
-// const client = createClient({
-//     password: 'nNfmacQ5KXHGSjsepfxPUto4gcu3QpeT',
-//     socket: {
-//         host: 'redis-11190.c278.us-east-1-4.ec2.redns.redis-cloud.com',
-//         port: 11190
-//     }
-// });
