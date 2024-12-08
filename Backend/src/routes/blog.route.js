7d190466367f5bb404c5c6b6a16b684df25d22ea
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-12-08 11:17:24 +0530

    update and add blogs count

--- Backend/src/routes/blog.route.js
+++ Backend/src/routes/blog.route.js
@@ -1,6 +1,5 @@
 import {
   createBlog,
-  //getBlog,
   getBlogList,
   allPostList,
   editPost,
@@ -10,28 +9,38 @@ import {
   popularPosts,
   trendingPosts,
   views,
-  
+  getBlogDashboardData,
 } from "../controllers/blog.controller.js";
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import { upload } from "../middlewares/multer.middleware.js";
 
 //const upload = multer()
 const router = Router();
 
-router.route("/createBlog").post( upload.fields([{name: "featuredImage", maxCount: 1}]), verifyJWT , createBlog);
+router
+  .route("/createBlog")
+  .post(
+    upload.fields([{ name: "featuredImage", maxCount: 1 }]),
+    verifyJWT,
+    createBlog
+  );
 //router.route("/api//:id").get(getBlog);
 router.route("/list").get(getBlogList);
-router.route("/allPostList").get(verifyJWT, allPostList)
+router.route("/allPostList").get(verifyJWT, allPostList);
 router.route("/editView/:id").get(verifyJWT, editView);
-router.route("/post/edit/:id").put( upload.fields([{name: "featuredImage", maxCount: 1}]), verifyJWT, editPost);
+router
+  .route("/post/edit/:id")
+  .put(
+    upload.fields([{ name: "featuredImage", maxCount: 1 }]),
+    verifyJWT,
+    editPost
+  );
 router.route("/delete/:id").delete(verifyJWT, deletePost);
-router.route("/draftpost").get(verifyJWT, draftPosts)
-router.route("/popular-posts").get(popularPosts)
-router.route("/trending-posts").get(trendingPosts)
-router.route("/post/views/:id").get(views)
-
-
-
+router.route("/draftpost").get(verifyJWT, draftPosts);
+router.route("/popular-posts").get(popularPosts);
+router.route("/trending-posts").get(trendingPosts);
+router.route("/post/views/:id").get(views);
+router.route("/blogs-count").get(verifyJWT, getBlogDashboardData);
 
 export default router;
