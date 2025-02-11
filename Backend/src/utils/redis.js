3770b0a6d9ebcfcacff9a1982310aa8414dfd625
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-11 19:17:21 +0530

    update redis credentials

--- Backend/src/utils/redis.js
+++ Backend/src/utils/redis.js
@@ -1,9 +1,9 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  password: 'Xg6lhNqqdCGL0OnsEIY1S9TqZraRLAFV',
-  host: 'redis-12308.c326.us-east-1-3.ec2.redns.redis-cloud.com',
-  port: 12308,
+  password: process.env.REDIS_PASSWORD,
+  host: 'redis-17852.c258.us-east-1-4.ec2.redns.redis-cloud.com',
+  port: 17852,
   username: "default",
   db: 0,
 });
