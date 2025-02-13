4226e293bd5fd3ba41a5d9e1dfa6720ac88ef823
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-13 10:53:54 +0530

    conditionally render SavedPosts based on user authentication

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import MainSidebar from "../component/MainSidebar";
 import PostList from "../component/PostList";
 import Carousel from "../component/Carousel.jsx";
-
+import { useAuth } from "../store/AuthContext.jsx";
 import SavedPosts from "../component/SavedPosts.jsx";
 
 export default function Home() {
+  const {user} = useAuth();
   return (
     <>
       <div className=" flex flex-col  lg:overflow-hidden lg:flex lg:flex-row bg-slate-50 text-cyan-400 dark:bg-gray-800 dark:text-gray-200">
@@ -19,7 +20,7 @@ export default function Home() {
 
         <div className=" lg:w-1/3 w-full">
           <MainSidebar />
-          <SavedPosts />
+          {user && <SavedPosts />}
         </div>
       </div>
     </>
