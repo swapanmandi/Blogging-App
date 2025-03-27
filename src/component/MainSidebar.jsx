9ea122212d06f5e1c836c6bc4a29e2bbf3961fa5
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-03-27 10:17:40 +0530

    refactor MainSidebar component for improved layout and readability

--- src/component/MainSidebar.jsx
+++ src/component/MainSidebar.jsx
@@ -27,7 +27,7 @@ export default function MainSidebar() {
   return (
     <>
       {loading ? (
-        <div className=" w-full m-2 rounded-xl">
+        <div className="w-full m-2 rounded-xl">
           <div className=" bg-white dark:bg-slate-950 min-h-screen p-3 rounded-md">
             <Skeleton height={30} width={100} />
             {[...Array(6)].map((_, index) => (
@@ -42,34 +42,25 @@ export default function MainSidebar() {
           </div>
         </div>
       ) : (
-        <div className="text-orange-500 w-full">
-          <aside className=" w-full dark:bg-slate-950 min-h-screen p-3 rounded-md">
-            <div className=" w-full text-wrap">
-              <h2 className="  bg-orange-300 font-semibold rounded-md p-1 w-fit">
-                Trending Posts
-              </h2>
-              {displayPosts?.map((item) => (
-                <Link
-                  key={item._id}
-                  className=" lg:w-full  h-28 m-2 p-1 flex lg:flex items-center lg:flex-row"
-                  to={`/blogs/${item._id}`}
-                >
-                  <div className=" min-w-[30%] w-[30%] h-24 lg:w-[88 px] lg:h-[88px] rounded-md m-1">
-                    <img
-                      className=" w-full h-full"
-                      src={item.featuredImage}
-                    ></img>
-                  </div>
-                  <div className=" bg-purple-500 w-full h-24 rounded-sm text-black m-1">
-                    <h2 className=" font-semibold font-serif m-4">
-                      {item.title}
-                    </h2>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </aside>
-        </div>
+        <aside className=" w-ful p-2 overflow-hidden dark:bg-slate-900 dark:text-white min-h-screen rounded-md ">
+          <h2 className="  bg-orange-300 font-semibold rounded-md p-1 w-fit">
+            Trending Posts
+          </h2>
+          {displayPosts?.map((item) => (
+            <Link
+              key={item._id}
+              className=" bg-purple-500 lg:w-full mt-3  h-28 gap-2 p-1 flex lg:flex items-center lg:flex-row"
+              to={`/blogs/${item._id}`}
+            >
+              <div className=" min-w-[30%] w-[30%] h-24 lg:w-[88 px] lg:h-[88px] rounded-md m-1">
+                <img className=" w-full h-full" src={item.featuredImage}></img>
+              </div>
+              <div className="w-full h-24 rounded-sm text-black dark:text-white m-1">
+                <h2 className=" font-semibold font-serif">{item.title}</h2>
+              </div>
+            </Link>
+          ))}
+        </aside>
       )}
     </>
   );
