7046d8ac10d51109efad092515b4a3974d63402c
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-03-27 10:18:02 +0530

    refactor RelatedPosts component for improved layout and styling

--- src/component/RelatedPosts.jsx
+++ src/component/RelatedPosts.jsx
@@ -7,14 +7,14 @@ export default function ({ posts, heading }) {
 
   return (
 
-      <div className=" w-full">
+      <div className=" relative w-full ">
         <h1 className=" font-bold text-lg p-1 m-2 bg-orange-400 rounded-md w-fit">
           {heading}
         </h1>
-        <div className=" w-full flex flex-col justify-center items-center lg:flex lg:flex-row">
+        <div className=" w-full h-full flex flex-col justify-center items-center lg:flex lg:flex-row">
           {relatedPosts?.map((item) => (
             <Link
-              className=" w-full bg-slate-200  flex flex-col items-center justify-center  dark:bg-slate-950 dark:text-white overflow-hidden p-2 lg:m-2 rounded-md  my-2"
+              className=" w-full bg-slate-200  flex flex-col items-center justify-center  dark:bg-slate-900 dark:text-white overflow-hidden p-2 lg:m-2 rounded-md  my-2"
               to={`/blogs/${item._id}`}
               key={item._id}
             >
